3b4a31547f94a4f7dcc9dfaad479771a36f110ea
DanielWang <51791301+DanielWang2002@users.noreply.github.com>
2025-11-21 12:33:43 +0800

    Initial Commit

---
+++ WorkPulse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorkPulse
+//
+//  Created by Daniel Wang on 2025/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
